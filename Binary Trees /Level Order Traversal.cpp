0ebd87b4aeb23cf7c2f416c7a8f4cce477c94e8d
Prisha <prisha.kumar@students.iiit.ac.in>
2023-07-02 16:57:58 +0530

    Create Level Order Traversal.cpp
    TC:O(N)
    SC:O(N)

---
+++ Binary Trees /Level Order Traversal.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>finalAnswer;
+        if(root==NULL)
+        {
+            return finalAnswer;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            vector<int>temp;
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* front=q.front();
+                q.pop();
+                temp.push_back(front->val);
+                if(front->left)
+                {
+                    q.push(front->left);
+                }
+                if(front->right)
+                {
+                    q.push(front->right);
+                }
+            }
+            finalAnswer.push_back(temp);
+        }
+        return finalAnswer;
+    }
+};
